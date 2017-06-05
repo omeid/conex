9c9fffe334a933d1e15412ccb3a4d13a0d72ed04
Omeid Matten <public@omeid.me>
2017-06-05 14:26:10 +1000

    postgres: Add a simple test

---
+++ postgres/postgres_test.go
@@ -0,0 +1,34 @@
+package postgres_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/omeid/conex"
+	"github.com/omeid/conex/postgres"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(conex.Run(m))
+}
+
+func TestMySQL(t *testing.T) {
+
+	sql, con := postgres.Box(t, &postgres.Config{
+		Database: "postgres",
+		User:     "postgres",
+	})
+	defer con.Drop()
+
+	var resp int
+	err := sql.QueryRow("SELECT 1").Scan(&resp)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp != 1 {
+		t.Fatal("Unexpected response: %v")
+	}
+
+}
