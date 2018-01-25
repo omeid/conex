e419d3526d98d4085ca93fc19bc2663aff5fb024
omeid matten <public@omeid.me>
2018-01-26 00:29:21 +1100

    ci: remove unused script, add mongodb to tests

--- ci/get_boxes.sh
+++ ci/get_boxes.sh
@@ -4,6 +4,7 @@ for box in                      \
   "github.com/conex/postgres"   \
   "github.com/conex/mysql"      \
   "github.com/conex/rethinkdb"  \
+  "github.com/conex/mongodb"    \
   "github.com/conex/postgres" 
 do
   go get -x -v $box
